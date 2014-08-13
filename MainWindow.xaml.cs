c09bf6aa15887e53cc8fa2256eda8de4a42ea52b
introsuit <introsuit@gmail.com>
2014-08-14 00:05:34 +0200

    threaded run; kill; refactor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,12 +12,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Reflection;
 using System.Threading;
 using System.Net;
-using System.Security.Principal;
+using ServiceLibrary;
 
 namespace LabRun
 {
@@ -26,21 +25,24 @@ namespace LabRun
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Service service = Service.getInstance();
+
         private string testfilepath = "";
         private string testfilename = "";
+
+        private string tempPath = System.IO.Path.GetTempPath();
+        private string labClientSharedFolder = @"C:\test\";
 
         public MainWindow()
         {
             InitializeComponent();
             initClients();
-            //testRun();
         }
 
         public void initClients()
         {
             string path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"clients.txt");
-            System.IO.StreamReader file =
-   new System.IO.StreamReader(path);
+            System.IO.StreamReader file = new System.IO.StreamReader(path);
 
             List<string> clients = new List<string>();
             string line;
@@ -48,39 +50,6 @@ namespace LabRun
             {
                 listBox1.Items.Add(line);
             }
-        }
-
-        public void printLDAPcomputers()
-        {
-            List<string> clients = GetComputers();
-            clients.Sort();
-            listBox1.ItemsSource = clients;
-        }
-
-
-        public static List<string> GetComputers()
-        {
-            List<string> ComputerNames = new List<string>();
-
-            DirectoryEntry entry = new DirectoryEntry("LDAP://asb.local", "donatas", "August2014");
-            DirectorySearcher mySearcher = new DirectorySearcher(entry);
-            mySearcher.Filter = ("(objectClass=computer)");
-            mySearcher.SizeLimit = int.MaxValue;
-            mySearcher.PageSize = int.MaxValue;
-
-            foreach (SearchResult resEnt in mySearcher.FindAll())
-            {
-                //"CN=SGSVG007DC"
-                string ComputerName = resEnt.GetDirectoryEntry().Name;
-                if (ComputerName.StartsWith("CN="))
-                    ComputerName = ComputerName.Remove(0, "CN=".Length);
-                ComputerNames.Add(ComputerName);
-            }
-
-            mySearcher.Dispose();
-            entry.Dispose();
-
-            return ComputerNames;
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
@@ -100,91 +69,108 @@ namespace LabRun
             {
                 // Open document 
                 string filename = dlg.FileName;
-                
+
                 int index = filename.LastIndexOf("\\");
                 if (index > 0)
                 {
-                    testfilepath = filename.Substring(0, index+1); // or index + 1 to keep slash
-                    testfilename = filename.Substring(index+1, filename.Length - (index+1)); // or index + 1 to keep slash
+                    testfilepath = filename.Substring(0, index); // or index + 1 to keep slash
+                    testfilename = filename.Substring(index + 1, filename.Length - (index + 1)); // or index + 1 to keep slash
                 }
 
                 label1.Content = filename;
                 button2.IsEnabled = true;
             }
-        }     
+        }
 
-        private void xcopy(List<string> computerNames, string srcDir, string dstDir)
+        private void xcopy(string srcDir, string dstDir)
         {
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\donatas\Desktop\testCopy.bat"))
+            string copyPath = tempPath + "testCopy.bat";
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(copyPath))
             {
                 file.WriteLine("@echo off");
-                foreach (string computerName in computerNames)
+                foreach (string computerName in getComputerNamesList())
                 {
-                    string line = @"xcopy """ + srcDir + @""" ""\\" + computerName+@"\test\" + dstDir + @""" /V /E /Y /Q /I";
+                    string line = @"xcopy """ + srcDir + @""" ""\\" + computerName + @"\test\" + dstDir + @""" /V /E /Y /Q /I";
                     file.WriteLine(line);
                 }
             }
+
+            service.ExecuteCommand(copyPath, true);
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-            List<string> computerNames = new  List<string>();
+            List<string> computerNames = new List<string>();
             foreach (string computerName in listBox1.SelectedItems)
             {
                 computerNames.Add(computerName);
             }
+
+            if (computerNames.Count == 0)
+            {
+                MessageBox.Show("Select some clients first!");
+                return;
+            }
+
             string newDir = Path.GetDirectoryName(testfilepath);
             newDir = newDir.Remove(0, newDir.LastIndexOf('\\') + 1);
 
-            xcopy(computerNames, testfilepath, newDir);          
+            xcopy(testfilepath, newDir);
+            runTests(computerNames, labClientSharedFolder + newDir + @"\" + testfilename);
+            btnKill.IsEnabled = true;
+            MessageBox.Show("Request done");
 
-            MessageBox.Show(@"C:\test\" + newDir + @"\" + testfilename);
-            runTests(computerNames, @"C:\test\" + newDir + @"\" + testfilename);
         }
 
-        private void runTests(List<string> computerNames, string testExePath){
-         
-            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\donatas\Desktop\testRun.bat"))
-            {
-                foreach (string computerName in computerNames)
-                {
-                    string line = @"C:\PSTools\PsExec.exe -d -i \\" + computerName + @" -u asb\labclient -p kPu$27mLi python " + testExePath;
-                    file.WriteLine(line);
-                 }
-            }
-
-            MessageBox.Show("will try to run");
-            string strCmdText;
-            strCmdText = @"C:\Users\donatas\Desktop\testRun.bat";
-            ExecuteCommand(strCmdText);
-        }
-
-        private void ExecuteCommand(string command)
+        public Thread StartNewCmdThread(string cmd)
         {
-            //int exitCode;
-            ProcessStartInfo processInfo;
-            Process process;
+            var t = new Thread(() => RealStart(cmd));
+            t.Start();
+            return t;
+        }
 
-            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
-            processInfo.CreateNoWindow = true;
-            processInfo.UseShellExecute = false;
-            // *** Redirect the output ***
-            // processInfo.RedirectStandardError = true;
-            // processInfo.RedirectStandardOutput = true;
+        private void RealStart(string cmd)
+        {
+            string strCmdText = @cmd;
+            service.ExecuteCommand(strCmdText);
+        }
 
-            process = Process.Start(processInfo);
-            //process.WaitForExit();
+        private void runTests(List<string> computerNames, string testExePath)
+        {
+            int i = 0;
+            foreach (string computerName in computerNames)
+            {
+                string runPath = tempPath + "testRun" + i + ".bat";
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(runPath))
+                {
 
-            // *** Read the streams ***
-            //string output = process.StandardOutput.ReadToEnd();
-            //string error = process.StandardError.ReadToEnd();
+                    //string line = @"C:\PSTools\PsExec.exe -d -i \\" + computerName + @" -u asb\labclient -p kPu$27mLi python " + testExePath;
+                    string line = @"C:\PSTools\PsExec.exe -d -i \\" + computerName + @" -u mano.local\Administrator -p Mandrass1 python " + testExePath;
+                    file.WriteLine(line);
+                }
 
-            //exitCode = process.ExitCode;
+                StartNewCmdThread(runPath);
 
-            //MessageBox.Show("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
-            //MessageBox.Show("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
-            //MessageBox.Show("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
-            process.Close();
+                i++;
+            }
+        }
+
+        private List<string> getComputerNamesList()
+        {
+            List<string> computerNames = new List<string>();
+            foreach (string computerName in listBox1.SelectedItems)
+            {
+                computerNames.Add(computerName);
+            }
+            return computerNames;
+        }
+
+        private void btnKill_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (string computerName in getComputerNamesList())
+            {
+                service.killRemoteProcess(computerName, "python.exe");
+            }
         }
     }
 }
