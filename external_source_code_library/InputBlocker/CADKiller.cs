9c5795498624d131dc0f1dc8226703955816d7c5
introsuit <introsuit@gmail.com>
2014-10-01 18:48:21 +0200

    included InputBlocker src code

---
+++ external_source_code_library/InputBlocker/CADKiller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputBlocker
+{
+    class CADKiller
+    {
+        private string path = @"Software\Microsoft\Windows\CurrentVersion\Policies\System";
+
+        public void KillCtrlAltDelete()
+        {
+            RegistryKey regkey;
+            string keyValueInt = "1";
+            string subKey = path;
+
+            regkey = Registry.CurrentUser.CreateSubKey(subKey);
+            regkey.SetValue("DisableTaskMgr", keyValueInt);
+            regkey.Close();
+        }
+
+        public void EnableCTRLALTDEL()
+        {
+            string subKey = path;
+            RegistryKey rk = Registry.CurrentUser;
+            RegistryKey sk1 = rk.OpenSubKey(subKey);
+            if (sk1 != null)
+                rk.DeleteSubKeyTree(subKey);
+        }
+    }
+}
---
+++ external_source_code_library/InputBlocker/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InputBlocker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CADKiller cadKiller;
+
+        public MainWindow()
+        {
+            //InitializeComponent();
+            KeyboardListener kListener = new KeyboardListener();
+            kListener.KeyDown += kListener_KeyDown;
+            //cadKiller = new CADKiller();
+            //cadKiller.KillCtrlAltDelete();
+        }
+
+        void kListener_KeyDown(object sender, RawKeyEventArgs args)
+        {
+            Debug.WriteLine(args.VKCode);
+        }
+
+        ~MainWindow()
+        {
+            if (cadKiller != null)
+                cadKiller.EnableCTRLALTDEL();
+        }
+    }
+}
